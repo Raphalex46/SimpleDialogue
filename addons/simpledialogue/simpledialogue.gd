b6fb45826c563dabe995103f4bb5ac6801618cf6
Raphaël Colin <raphael.colin68@gmail.com>
2025-10-28 00:17:56 +0100

    icons: created icons for SimpleDialogue nodes and resources

--- addons/simpledialogue/simpledialogue.gd
+++ addons/simpledialogue/simpledialogue.gd
@@ -17,6 +17,12 @@ func _enter_tree() -> void:
 	preload("dialogue_holder/dialogue_holder.gd"), preload("dialogue_holder/icon.svg"))
 	add_custom_type("DialogueDisplay", "RichTextLabel",
 	preload("dialogue_display/dialogue_display.gd"), preload("dialogue_display/icon.svg"))
+	add_custom_type("DialogueLine", "Resource",
+	preload("resource_scripts/dialogue_line.gd"),
+	preload("resource_scripts/dialogue_line_icon.svg"))
+	add_custom_type("Character", "Resource",
+	preload("resource_scripts/character.gd"),
+	preload("resource_scripts/character_icon.svg"))
 
 func _exit_tree() -> void:
 	remove_custom_type("DialogueHolder")
